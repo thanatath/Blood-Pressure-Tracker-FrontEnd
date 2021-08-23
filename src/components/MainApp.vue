40dc44b27ffaa661e41f02987b9678a8db7db2e2
THANATATH ASSAVARATTANAKORN <62015048@kmitl.ac.th>
2021-08-23 15:59:40 +0700

    fix heartrate don't reset when send data

--- src/components/MainApp.vue
+++ src/components/MainApp.vue
@@ -4,7 +4,14 @@
       <div class="col-sm-4">
         <div class="input-group mb-3">
           <div class="input-group-prepend">
-            <span class="input-group-text" id="basic-addon1"><i style="margin-right:7px" class="fa fa-bolt" aria-hidden="true"></i>TOP</span>
+            <span class="input-group-text" id="basic-addon1"
+              ><i
+                style="margin-right:7px"
+                class="fa fa-bolt"
+                aria-hidden="true"
+              ></i
+              >TOP</span
+            >
           </div>
           <input
             v-model="top"
@@ -19,7 +26,14 @@
       <div class="col-sm-4">
         <div class="input-group mb-3">
           <div class="input-group-prepend">
-            <span class="input-group-text" id="basic-addon1"><i style="margin-right:7px" class="fa fa-hourglass-end" aria-hidden="true"></i>Buttom</span>
+            <span class="input-group-text" id="basic-addon1"
+              ><i
+                style="margin-right:7px"
+                class="fa fa-hourglass-end"
+                aria-hidden="true"
+              ></i
+              >Buttom</span
+            >
           </div>
           <input
             v-model="buttom"
@@ -31,10 +45,17 @@
           />
         </div>
       </div>
-            <div class="col-sm-4">
+      <div class="col-sm-4">
         <div class="input-group mb-3">
           <div class="input-group-prepend">
-            <span class="input-group-text" id="basic-addon1"><i style="margin-right:7px" class="fa fa-heartbeat" aria-hidden="true"></i> HeartRate</span>
+            <span class="input-group-text" id="basic-addon1"
+              ><i
+                style="margin-right:7px"
+                class="fa fa-heartbeat"
+                aria-hidden="true"
+              ></i>
+              HeartRate</span
+            >
           </div>
           <input
             v-model="heartRate"
@@ -64,10 +85,47 @@
           <div class="card-header">
             <center>History Record</center>
           </div>
-          <ul v-for="allRecord in allRecord.data" :key="allRecord.data" class="list-group list-group-flush">
-             
-             <center><li class="list-group-item"><i style="margin-right:7px" class="fa fa-bolt" aria-hidden="true"></i> {{allRecord.top}} <i style="margin-right:7px;margin-left:19px" class="fa fa-hourglass-end" aria-hidden="true"></i>  {{allRecord.buttom}} <i style="margin-right:7px;margin-left:19px" class="fa fa-heartbeat" aria-hidden="true"></i>  {{allRecord.heartrate}}  <i style="margin-right:7px;margin-left:19px" class="fa fa-calendar" aria-hidden="true"></i> {{allRecord.time}}  <button @click="delData(allRecord.id)" style="margin-left:35px;;margin-left:19px" type="button" class="btn btn-danger btn-sm">Delete</button> </li> </center>
-            
+          <ul
+            v-for="allRecord in allRecord.data"
+            :key="allRecord.data"
+            class="list-group list-group-flush"
+          >
+            <center>
+              <li class="list-group-item">
+                <i
+                  style="margin-right:7px"
+                  class="fa fa-bolt"
+                  aria-hidden="true"
+                ></i>
+                {{ allRecord.top }}
+                <i
+                  style="margin-right:7px;margin-left:19px"
+                  class="fa fa-hourglass-end"
+                  aria-hidden="true"
+                ></i>
+                {{ allRecord.buttom }}
+                <i
+                  style="margin-right:7px;margin-left:19px"
+                  class="fa fa-heartbeat"
+                  aria-hidden="true"
+                ></i>
+                {{ allRecord.heartrate }}
+                <i
+                  style="margin-right:7px;margin-left:19px"
+                  class="fa fa-calendar"
+                  aria-hidden="true"
+                ></i>
+                {{ allRecord.time }}
+                <button
+                  @click="delData(allRecord.id)"
+                  style="margin-left:35px;;margin-left:19px"
+                  type="button"
+                  class="btn btn-danger btn-sm"
+                >
+                  Delete
+                </button>
+              </li>
+            </center>
           </ul>
         </div>
       </div>
@@ -84,52 +142,84 @@ export default {
     return {
       top: '',
       buttom: '',
-      allRecord:'',
-      heartRate:'',
-
+      allRecord: '',
+      heartRate: '',
     };
   },
   methods: {
-    getData(){
-            axios
-        .get("http://"+process.env.VUE_APP_BACKENDHOST+":"+process.env.VUE_APP_BACKENDPORT+"/history")
+    getData() {
+      axios
+        .get(
+          'http://' +
+            process.env.VUE_APP_BACKENDHOST +
+            ':' +
+            process.env.VUE_APP_BACKENDPORT +
+            '/history'
+        )
         .then((response) => {
-          this.allRecord = response})}
-          
-    ,
+          this.allRecord = response;
+        });
+    },
+
     checkdata() {
-      return !(parseInt(this.top) > 0 && parseInt(this.buttom) > 0 && parseInt(this.heartRate) > 0);
+      return !(
+        parseInt(this.top) > 0 &&
+        parseInt(this.buttom) > 0 &&
+        parseInt(this.heartRate) > 0
+      );
     },
     sendData() {
       axios
-        .get("http://"+process.env.VUE_APP_BACKENDHOST+":"+process.env.VUE_APP_BACKENDPORT+'?top=' + this.top + '&buttom=' + this.buttom+ '&heartrate=' + this.heartRate)
+        .get(
+          'http://' +
+            process.env.VUE_APP_BACKENDHOST +
+            ':' +
+            process.env.VUE_APP_BACKENDPORT +
+            '?top=' +
+            this.top +
+            '&buttom=' +
+            this.buttom +
+            '&heartrate=' +
+            this.heartRate
+        )
         .then((response) => {
           if (response.data == 'OK') {
             this.$alert('Save Blood Data Complete.');
           } else {
             this.$alert('Save Blood Data Not Complete.');
           }
-          (this.top = ''), (this.buttom = '');
+          this.resetInput()
         });
-        this.getData()
+      this.getData();
     },
-    delData(delId){
+    resetInput() {
+      this.top = '';
+      this.buttom = '';
+      this.heartRate ='';
+    },
+    delData(delId) {
       axios
-        .get("http://"+process.env.VUE_APP_BACKENDHOST+":"+process.env.VUE_APP_BACKENDPORT+'/del?idBlood=' + delId)
+        .get(
+          'http://' +
+            process.env.VUE_APP_BACKENDHOST +
+            ':' +
+            process.env.VUE_APP_BACKENDPORT +
+            '/del?idBlood=' +
+            delId
+        )
         .then((response) => {
           if (response.data == 'OK') {
             this.$alert('Delete Blood Data Complete.');
           } else {
             this.$alert('Delete Blood Data Not Complete.');
           }
-          (this.top = ''), (this.buttom = '');
+          this.resetInput();
         });
-        this.getData()
+      this.getData();
     },
   },
   mounted() {
-    this.getData()
-     
+    this.getData();
   },
 };
 </script>
